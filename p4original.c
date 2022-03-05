85f1f171a90c331b58f3cd6ba4f0d87d39cc636e
shyam003 <94587669+Shyamsnair@users.noreply.github.com>
2022-03-05 06:49:10 +0000

    .

---
+++ p4original.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the number of input");
+  scanf("%d",&n);
+  return n;
+  }
+void input_array(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      printf("enter an element:\n");
+      scanf("%d",&a[i]);
+      }
+  }
+int sum_composite_numbers(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+    {
+    for(int j=2;j<=a[i]/2;j++)
+      {
+        if(a[i]%j==0)
+        {
+          sum=sum+a[i];
+          break;
+          }
+        else
+        {
+          continue;
+          }
+        }
+      }
+  return sum;
+  }
+void out_put(int sum)
+{
+  printf("%d is the output",sum);
+  }
+int main()
+{
+int n,sum;
+  n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  sum=sum_composite_numbers(n,a);
+  out_put(sum);
+  return 0;
+  }
